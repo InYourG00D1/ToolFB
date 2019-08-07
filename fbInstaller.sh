1104dee01f829838634aca44f9675c9537cfa613
InYourG00D1 <44290386+InYourG00D1@users.noreply.github.com>
2019-08-07 16:33:57 +0700

    Add files via upload

---
+++ fbInstaller.sh
@@ -0,0 +1,349 @@
+#!/system/xbin/bash
+
+blue='\033[34;1m'
+green='\033[32;1m'      
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'             
+yellow='\033[33;1m'
+
+echo "\033[34;1m]
+  ____  ___
+ |  __|| 0 )
+ |  _| |   \ |    ___ __| 0 |
+ | |   | 0. )|-  /  /  /|/|>|<
+ | |   |   / |__/__/__/ |\| |  installer
++ coded by : InYourG00D                   +
++ Team : LNX#CREW S.T.C                   +
++ GitHub : https://github.com/InYourG00D1 +
+    ^...^             ^...^
+   <_* *_> FBtoolkit <_* *_> 
+     \_/               \_/  
+$white "[!] If your account has been hit by a logger, report me immediately"
+";
+   
+echo $red"| 1  |" MessengerBot $green "[ON]"
+echo $red"| 2  |" Encrytor $green "[ON]"
+echo $red"| 3  |" DeleteFB $green "[ON]"
+echo $red"| 4  |" MessengerScaper $green "[ON]"
+echo $red"| 5  |" Dark FB $green "[ON]"
+echo $red"| 6  |" BruteForce $green "[ON]"
+echo $red"| 7  |" Terminal On FB Messenger $green "[ON]"
+echo $red"| 8  |" pyMessenger $green "[ON]"
+echo $red"| 9  |" FBLog $green "[ON]"
+echo $red"| 10 |" FBEraser $green "[ON]"
+echo $red"| 11 |" FBBot $green "[ON]"
+echo $red"| 12 |" FBspider $green "[ON]"
+echo $red"| 13 |" FBBrute $green "[ON]"
+echo $red"| 14 |" AutoReport $green "[ON]"
+echo $red"| 15 |" PageChatDownload $green "[ON]"
+echo $red"| 16 |" Message-Parser $green "[ON]"
+echo $red"| 17 |" ModBot $green "[ON]"
+echo $red"| 18 |" FBpyGIF $green "[ON]"
+echo $red"| 19 |" PhotoDownloader $green "[ON]"
+echo $red"| 20 |" LinkScraper $green "[ON]"
+echo $red"| 21 |" DarkFB 1.6 $green "[ON]"
+echo $red"| 22 |" FBProfilePictureDump $green "[ON]"
+echo $red"| 23 |" FBMaker $green "[ON]"
+echo $red"| 24 |" FBChatBot $green "[ON]"
+echo $red"| 25 |" FBUnfriend $green "[ON]"
+echo $red"| 26 |" VideoDownloader $green "[ON]"
+echo $red"| 27 |" ChatBot $green "[ON]"
+echo $red"| 28 |" FBChecker $green "[ON]"
+echo $red"| 29 |" FBVideoDownloader $green "[ON]"
+echo $red"| 30 |" MBF-1 $green "[ON]"
+echo $red"| 31 |" MBF $green "[ON]"
+echo $red"| 32 |" DarkFB $green "[ON]"
+echo $red"| 33 |" FBGraph $green "[ON]"
+echo $red"| 34 |" FBCMD $green "[ON]"
+echo $red"| 35 |" FBGallery $green "[ON]"
+echo $red"| 36 |" FBMessengerNode $green "[ON]"
+echo $red"| 37 |" FBMessengerSDK $green "[ON]"
+echo $red"| 38 |" FBForMagento $green "[ON]"
+echo $red"| 39 |" FBAutoreaction $green "[ON]"
+echo $red"| 40 |" FBGuard $green "[ON]"
+echo $red"| 41 |" FBPhising $green "[ON]"
+echo $red"| 42 |" FBDev $green "[ON]"
+echo $red"| 43 |" FBApp $green "[ON]"
+echo $red"| 44 |" FBAutoPost $green "[ON]"
+echo $red"| 45 |" FBphish $green "[ON]"
+echo $red"| 00 |" Update 45-80tools $green "[OFF]"
+read -p ">" au
+
+if [ $au = 1 ]
+then
+clear
+git clone https://github.com/hartleybrody/fb-messenger-bot
+fi
+
+if [ $au = 2 ]
+then
+clear
+git clone git clone https://github.com/dev5tec/FBEncryptor
+fi
+
+if [ $au = 3 ]
+then
+clear
+git clone https://github.com/weskerfoot/DeleteFB
+fi
+
+if [ $au = 4 ]
+then
+clear
+git clone https://github.com/RaghavSood/FBMessageScraper
+fi
+
+if [ $au = 5 ]
+then
+clear
+git clone https://github.com/rezadkim/dark-fb
+fi
+
+if [ $au = 6 ]
+then
+clear
+git clone https://github.com/Senitopeng/fbbrute
+fi
+
+if [ $au = 7 ]
+then
+clear
+git clone https://github.com/dhruvramani/Terminal-on-FB-Messenger
+fi
+
+if [ $au = 8 ]
+then
+clear
+git clone https://github.com/davidchua/pymessenger
+fi
+
+if [ $au = 9 ]
+then
+clear
+git clone https://github.com/ghost123gg/fblog
+fi
+
+if [ $au = 10 ]
+then
+clear
+git clone https://github.com/asonawalla/FBEraser
+fi
+
+if [ $au = 11 ]
+then
+clear
+git clone https://github.com/masnun/fb-bot
+fi
+
+if [ $au = 12 ]
+then
+clear
+git clone https://github.com/Parth-Vader/FB-Spider
+fi
+
+if [ $au = 13 ]
+then
+clear
+git clone https://github.com/Gameye98/FBBrute
+fi
+
+if [ $au = 14 ]
+then
+clear
+git clone https://github.com/SIIL3NT/auto-report
+fi
+
+if [ $au = 15 ]
+then
+clear
+git clone https://github.com/eisenjulian/fb-page-chat-download
+fi
+
+if [ $au = 16 ]
+then
+clear
+git clone https://github.com/CopOnTheRun/FB-Message-Parser
+fi
+
+if [ $au = 17 ]
+then
+clear
+git clone https://github.com/ZacSweers/FB_Mod_Bot
+fi
+
+if [ $au = 18 ]
+then
+clear
+git clone https://github.com/chidea/FBpyGIF
+fi
+
+if [ $au = 19 ]
+then
+clear
+git clone https://github.com/jcontini/fb-photo-downloader
+fi
+
+if [ $au = 20 ]
+then
+clear
+git clone https://github.com/iCHAIT/FBLinkScraper
+fi
+
+if [ $au = 21 ]
+then
+clear
+git clone https://github.com/ZeDDParker/dark-fb
+fi
+
+if [ $au = 22 ]
+then
+clear
+git clone https://github.com/sahildua2305/fb-profile-picture-dump
+fi
+
+if [ $au = 23 ]
+then
+clear
+git clone https://github.com/zevtyardt/fb-maker
+fi
+
+if [ $au = 24 ]
+then
+clear
+git clone https://github.com/kadnan/Facebook-Chat-Bot
+fi
+
+if [ $au = 25 ]
+then
+clear
+git clone https://github.com/dandyraka/FacebookUnfriend
+fi
+
+if [ $au = 26 ]
+then
+clear
+git clone https://github.com/irazasyed/video-downloader
+fi
+
+if [ $au = 27 ]
+then
+clear
+git clone https://github.com/paulooosrj/chatbot-php-facebook
+fi
+
+if [ $au = 28 ]
+then
+clear
+git clone https://github.com/fdciabdul/fbchecker
+fi
+
+if [ $au = 29 ]
+then
+clear
+git clone https://github.com/bcat95/Facebook-Video-Downloader
+fi
+
+if [ $au = 30 ]
+then
+clear
+git clone https://github.com/zapnu/mbf-1
+fi
+
+if [ $au = 31 ]
+then
+clear
+git clone https://github.com/YukersCreew/mbf
+fi
+
+if [ $au = 32 ]
+then
+clear
+git clone https://github.com/JeelsBoobz/DarkFB
+fi
+
+if [ $au = 33 ]
+then
+clear
+git clone https://github.com/nsanta/fbgraph
+fi
+
+if [ $au = 34 ]
+then
+clear
+git clone https://github.com/dtompkins/fbcmd
+fi
+
+if [ $au = 35 ]
+then
+clear
+git clone https://github.com/Seitk/FB-Gallery
+fi
+
+if [ $au = 36 ]
+then
+clear
+git clone https://github.com/rickydunlop/fbmessenger-node
+fi
+
+if [ $au = 37 ]
+then
+clear
+git clone https://github.com/tgallice/fb-messenger-sdk
+fi
+
+if [ $au = 38 ]
+then
+clear
+git clone https://github.com/facebookincubator/facebook-for-magento
+fi
+
+if [ $au = 39 ]
+then
+clear
+git clone https://github.com/tomiashari/fb-autoreaction
+fi
+
+if [ $au = 40 ]
+then
+clear
+git clone https://github.com/Al2VyN/FB-Guard
+fi
+
+if [ $au = 41 ]
+then
+clear
+git clone https://github.com/johnjoseph/fb-Phishing
+fi
+
+if [ $au = 42 ]
+then
+clear
+git clone https://github.com/lordspace/fbdev
+fi
+
+if [ $au = 43 ]
+then
+clear
+git clone https://github.com/kumpulanremaja/pp-fb
+fi
+
+if [ $au = 44 ]
+then
+clear
+git clone https://github.com/kastrind/AutoFbPost
+fi
+
+if [ $au = 45 ]
+then
+clear
+git clone https://github.com/0xyg3n/fbphish
+fi
+
+if [ $au = 00 ]
+then
+clear
+echo $white "Kontak Me : 0895805133060"
+echo $white "Use Your Brain"
+fi
